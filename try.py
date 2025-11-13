be1c12a9f58bcadcdf1bc88025018de69cae1783
Epiccraze <Pal_rao@yahoo.com>
2025-11-12 19:52:57 -0600

    Add files via upload

--- try.py
+++ try.py
@@ -5,8 +5,6 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error: Could not open camera")
     exit()
-else:
-    print("Camera successfully opened")
 blink_count = 0
 eyes_closed_frames = 0
 EYE_CLOSED_FRAMES_THRESHOLD = 3
@@ -27,3 +25,11 @@ while True:
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
         else:
             eyes_closed_frames += 1
+            if eyes_closed_frames == EYE_CLOSED_FRAMES_THRESHOLD:
+                blink_count += 1
+    cv2.putText(frame, f'Blinks: {blink_count}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+    cv2.imshow('Face & Blink Detection', frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
